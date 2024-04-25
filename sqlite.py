441040e4722a5cae089a28248320d73d3841ae17
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-25 09:00:33 -0600

    Add sqlite pratice

---
+++ sqlite.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+# Create the database, inside  () is name of database
+connection =  sqlite3.connect("gta.db")
+
+release_list = [
+  (1997, "Grand Theft Auto", "state of New Guernsey"),
+  (1999, "Grand Theft Auto 2", "Anywhere, USA"),
+  (2001, "Grand Theft Auto III", "Libery, City"),
+  (2002, "Grand Theft Auto: Vice City", "Vice City"),
+  (2004, "Grand Theft Auto: San Andreas", "state of San Andreas"),
+  (2008, "Grand Theft Auto IV", "Libery City"),
+  (201, "Grand Theft Auto V", "Los Santos"),
+]
+
+connection.close()
